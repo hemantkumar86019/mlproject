969e968ccff11ec629efc68ae8c468fa7dbad06a
hemantkumar86019 <hemant.86019@gmail.com>
2025-06-04 12:20:10 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,32 +47,71 @@ class ModelTrainer:
                 "Cat Boosting Regressor": CatBoostRegressor()
             }
 
-            model_report:dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models)
+            params = {
+                "Random Forest": {
+                    "n_estimators": [50, 100],
+                    # "max_depth": [None, 10, 20],
+                    # "criterion": ["squared_error", "absolute_error"]
+                },
+                "Decision Tree": {
+                    # "max_depth": [5, 10, 20],
+                    "criterion": ["squared_error", "absolute_error"]
+                },
+                "Gradient Boosting": {
+                    "n_estimators": [50, 100],
+                    "learning_rate": [0.01, 0.1],
+                    # "loss": ["squared_error", "absolute_error"]
+                },
+                "Linear Regressor": {},  # No tuning required
+                "K-Neighbours Regressor": {
+                    "n_neighbors": [3, 5, 7]
+                },
+                "XG-Boost Regressor": {
+                    "n_estimators": [50, 100],
+                    "learning_rate": [0.01, 0.1],
+                    # "objective": ["reg:squarederror"]
+                },
+                "Ada Boost Regressor": {
+                    "n_estimators": [50, 100],
+                    "learning_rate": [0.01, 0.1],
+                    "loss": ["linear", "square"]
+                },
+                "Cat Boosting Regressor": {
+                    "iterations": [50, 100],
+                    "depth": [4, 6],
+                    "learning_rate": [0.01, 0.1],
+                    # "loss_function": ["RMSE", "MAE"],
+                    # "verbose": [0]  # suppress CatBoost output
+                }
+            }
 
-            ## to get best model score from dict
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,params=params)
+            
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-            ## to get best model name from dict
+            ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
             best_model = models[best_model_name]
 
-            if best_model_score < 0.6:
+            if best_model_score<0.6:
                 raise CustomException("No best model found")
-            
             logging.info(f"Best found model on both training and testing dataset")
-            
+
             save_object(
-                file_path = self.model_trainer_config.trained_model_file_path,
-                obj = best_model
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
             )
 
-            predicted = best_model.predict(X_test)
+            predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
+
         
 
             
